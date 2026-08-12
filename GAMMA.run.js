15387baffacadeaae6f300d96cbc1a1a093f60f7
iki1uc <entertrainer@web.de>
2026-08-12 15:46:10 +0200

    Update GAMMA.run.js

--- GAMMA.run.js
+++ GAMMA.run.js
@@ -1,10 +1,17 @@
-export function V3_THIRD(){
-  const X4 = 1/3;
-  const IX = 1/3;
-  const XI = 1/3;
+import { AXIOM_THIRD } from "./tmp.a";
+import { V3_THIRD } from "./V3_THIRD.js";
+
+formulas(alpha, beta) {
+  const v3 = V3_THIRD();
 
   return {
-    sum: X4 + IX + XI,
-    core: X4 * IX * XI
+    third_axiom: AXIOM_THIRD.value,
+    third_sum: v3.sum,
+    third_core: v3.core,
+    iki1uc_vector: `V3 = ${v3.core}`,
+    alpha_degree: alpha.degree,
+    alpha_percent: alpha.percent,
+    beta_code: beta.code,
+    beta_tech: beta.tech
   };
 }
