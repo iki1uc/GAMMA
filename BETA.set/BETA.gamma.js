7c6586cd01618b17f1fbedd781e69e4435657c49
iki1uc <entertrainer@web.de>
2026-08-12 14:54:42 +0200

    Update BETA.gamma.js

--- BETA.set/BETA.gamma.js
+++ BETA.set/BETA.gamma.js
@@ -1,1 +1,61 @@
+export const BETA_gamma = {
+  id: "BETA.gamma",
+  mode: "check",
+  status: "active",
+
+  check(alpha) {
+    return {
+      code: this.code(alpha),
+      tech: this.tech(alpha),
+      vec: this.vec(alpha),
+      tmp: this.tmp(alpha),
+      rotation: this.rotation(alpha),
+      load: this.load(alpha),
+      output: this.output(alpha),
+      degree: this.degree(alpha),
+      percent: this.percent(alpha),
+      ready: this.ready(alpha)
+    };
+  },
+
+  code(alpha) {
+    return alpha ? "OK" : "ERR";
+  },
+
+  tech(alpha) {
+    return alpha.tech === true ? "OK" : "ERR";
+  },
+
+  vec(alpha) {
+    return alpha.vec ? "OK" : "ERR";
+  },
+
+  tmp(alpha) {
+    return alpha.tmp ? "OK" : "ERR";
+  },
+
+  rotation(alpha) {
+    return alpha.degree === "360°" ? "FULL" : "HALF";
+  },
+
+  load(alpha) {
+    return alpha.load ? "OK" : "ERR";
+  },
+
+  output(alpha) {
+    return alpha.output ? "OK" : "ERR";
+  },
+
+  degree(alpha) {
+    return alpha.degree || "0°";
+  },
+
+  percent(alpha) {
+    return alpha.percent || "0%";
+  },
+
+  ready(alpha) {
+    return alpha.degree === "360°" && alpha.percent === "100%" ? true : false;
+  }
+};
 
